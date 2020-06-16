c1c9ff2ef01f8b5749b087dd427066e9b6a7b1c0
Ricardo Ribeiro <ricardorqr@gmail.com>
2020-06-16 19:57:44 -0400

    Initial Version

---
+++ src/main/java/com/simpleSpringBootECommerce/SimpleSpringBootECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.simpleSpringBootECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpleSpringBootECommerce/SimpleSpringBootECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleSpringBootECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
